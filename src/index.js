79aca6697fbd8269cc28b6cdacbc44200019554c
Martina <martina.holenstein@gmail.com>
2024-04-04 15:30:02 +0200

    add fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Alpine from 'alpinejs';
 import Swup from 'swup';
+import Glide from "@glidejs/glide";
 import { animate } from "motion"
 
 // import style for js
@@ -11,3 +12,28 @@ Alpine.start();
 
 // SWUP
 const swup = new Swup();
+
+if (document.querySelector(".glide")) {
+    let glide = new Glide(".glide", {
+      type: "carousel",
+      startAt: 0,
+      perView: 1,
+      gap: 5,
+      peek: {
+        before: 15,
+        after: window.innerWidth / 5,
+      },
+    });
+  
+    glide.mount();
+  
+    window.addEventListener("resize", function () {
+      glide.update({
+        peek: {
+          before: 15,
+          after: window.innerWidth / 5,
+        },
+      });
+    });
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["GeigyLLMedium", "sans-serif"],
+        serif: ["TimesTen", "serif"],
+      },
     },
   },
   variants: {},
