5495f80458f9de6969402d7466a2081f0d9f7f36
Guido Offermans <dev@guidooffermans.com>
2024-05-19 13:16:28 +0200

    clean up home and project templates

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,155 +4,33 @@
 <?php snippet('head') ?>
 
 <body class=" h-full grid grid-rows-12 bg:white dark:bg-black">
-  <header class="row-span-3">
-    <nav class="flex gap-16 font-serif ">
-      <h2 class="ml-3"><?php snippet('projectindex') ?></h2>
-      <?php snippet('about') ?>
-    </nav>
+<header class="row-span-3">
+  <nav class="flex gap-16 font-serif ">
+    <h2 class="ml-3"><?php snippet('projectIndex') ?></h2>
+    <?php snippet('about') ?>
+  </nav>
+</header>
 
-  </header>
+<main class="lg:row-span-6">
+  <ul
+    class="flex flex-col lg:flex-row lg:overflow-x-scroll gap-1 lg:gap-10 w-full items-center px-12 lg:px-0 lg:h-full no-scrollbar scroll-smooth snap-y lg:snap-x snap-mandatory pb-24 lg:pb-0">
+    <?php foreach ($projects as $project): ?>
+      <figure class="h-full flex-none snap-always snap-center" data-project='<?= json_encode($project) ?>'>
+        <?= $project->image->thumb([
+          'quality' => 90,
+          'format' => 'webp',
+        ])->html(); ?>
+      </figure>
+    <?php endforeach;
+    ?>
+  </ul>
 
-  <main class="lg:row-span-6">
-    <ul
-      class="flex flex-col lg:flex-row lg:overflow-x-scroll  gap-1 lg:gap-10 w-full items-center px-12 lg:px-0 lg:h-full no-scrollbar scroll-smooth snap-y lg:snap-x snap-mandatory pb-24 lg:pb-0">
-      <?php $projectsPage = $site->find('projects');
-      foreach ($projectsPage->children()->shuffle() as $project) {
+  <?php snippet('projectDrawer') ?>
 
-        $hslColor = $project->backgroundColor()->escape();
-        $alpha = 0.8; // 80% opacity
-        $hslColor = trim($hslColor);
-        $hslColor = substr($hslColor, 4, -1);
-        $hslParts = explode(' ', $hslColor);
-        $hue = trim($hslParts[0]);
-        $saturation = trim($hslParts[1]);
-        $lightness = trim($hslParts[2]);
-        $hslaColor = "hsla($hue, $saturation, $lightness, $alpha)";
+</main>
 
-        $kirbyPressKits = $project->pressKits()->toStructure();
-        $pressKits = [];
-
-        foreach ($kirbyPressKits as $pressKit): ?>
-          <?php array_push($pressKits, (object) [
-            "title" => $pressKit->title()->value(),
-            "link" => $pressKit->link()->value(),
-            "toggle" => $pressKit->toggle()->value(),
-          ]) ?>
-        <?php endforeach;
-
-        $projectInfo = (object) [
-          "title" => $project->title()->value(),
-          "url" => $project->url(),
-          "description" => $project->description()->value(),
-          "backgroundColor" => $hslaColor,
-          "pressKits" => $pressKits
-        ];
-
-        foreach ($project->cover()->toFiles() as $image): ?>
-          <figure class="h-full flex-none snap-always snap-center" data-project='<?= json_encode($projectInfo) ?>'>
-            <?= $image->thumb([
-              'quality' => 90,
-              'format' => 'webp',
-            ])->html(); ?>
-          </figure>
-        <?php endforeach;
-
-        foreach ($project->gallery()->toFiles() as $image): ?>
-          <figure class="h-full flex-none snap-always snap-center" data-project='<?= json_encode($projectInfo) ?>'>
-            <?= $image->thumb([
-              'quality' => 90,
-              'format' => 'webp',
-            ])->html(); ?>
-          </figure>
-        <?php endforeach;
-      }
-      ?>
-    </ul>
-
-    <div class="">
-      <?php snippet('project') ?>
-    </div>
-
-  </main>
-
-  <script>
-    const baseOpacity = "0.90";
-
-    document.addEventListener('DOMContentLoaded', function () {
-      const imageElements = document.querySelectorAll('main figure img');
-
-      imageElements.forEach((imageElement) => {
-        imageElement.style.opacity = baseOpacity
-        imageElement.closest('figure').addEventListener('mouseleave', function () {
-          imageElements.forEach(img => {
-            img.style.opacity = baseOpacity;
-          })
-        });
-
-        imageElement.closest('figure').addEventListener('mouseover', function () {
-          imageElements.forEach(otherImageElements => {
-            if (otherImageElements !== imageElement) {
-              otherImageElements.style.opacity = baseOpacity;
-            }
-            imageElement.style.opacity = "1";
-          })
-
-          const data = JSON.parse(this.dataset.project);
-          showActiveProjectShowMoreTitle(data.title)
-          showActiveProjectInfoOnDrawer(data)
-        });
-        correctFigureSize(imageElement)
-      });
-    });
-
-    function correctFigureSize(figureElement) {
-      figureElement.onload = function () {
-        const aspectRatio = figureElement.naturalWidth / figureElement.naturalHeight;
-        const containerHeight = figureElement.closest('figure').clientHeight;
-        figureElement.closest('figure').style.width = `${containerHeight * aspectRatio}px`;
-      };
-    }
-
-    function showActiveProjectShowMoreTitle(title) {
-      document.getElementById("project-more-info").innerText = title
-    }
-
-    // Set the correct information on the project drawer
-    function showActiveProjectInfoOnDrawer({ title, description, backgroundColor, pressKits, url }) {
-      // Set the project title
-      document.getElementById("project-title").innerText = title
-      // Set the background color
-      document.getElementById("navigation").style.backgroundColor = backgroundColor
-      // Set the share link
-      document.getElementById("project-url").href = url
-
-      // Clear and set the description
-      const descriptionElement = document.getElementById("project-description")
-      while (descriptionElement.firstChild) {
-        descriptionElement.removeChild(descriptionElement.firstChild);
-      }
-      const parser = new DOMParser();
-      const descriptionNodes = parser.parseFromString(description, "text/html");
-      Array.from(descriptionNodes.body.childNodes).forEach(item => descriptionElement.appendChild(item.cloneNode(true))
-      )
-
-      // Clear and set the presskit links
-      const presskitsElement = document.getElementById("project-presskits")
-      while (presskitsElement.firstChild) {
-        presskitsElement.removeChild(presskitsElement.firstChild);
-      }
-      pressKits.forEach(kit => {
-        const anchorTag = document.createElement('a')
-        anchorTag.href = kit.link
-        anchorTag.innerText = kit.title
-        if (!!kit.toggle) {
-          anchorTag.target = "_blank"
-        }
-        presskitsElement.appendChild(anchorTag.cloneNode(true))
-      })
-    }
-  </script>
-
-  <?= vite()->js('index.js') ?>
+<?= vite()->js('index.js') ?>
+<?= vite()->js('home.js') ?>
 </body>
 
 </html>
--- site/templates/project.php
+++ site/templates/project.php
@@ -3,107 +3,99 @@
 
 <?php snippet('head') ?>
 <?php
-$hslColor = $page->backgroundColor()->escape();
-$alpha = 1; // 50% opacity
-$hslColor = trim($hslColor);
-$hslColor = substr($hslColor, 4, -1);
-$hslParts = explode(' ', $hslColor);
-$hue = trim($hslParts[0]);
-$saturation = trim($hslParts[1]);
-$lightness = trim($hslParts[2]);
-$hslaColor = "hsla($hue, $saturation, $lightness, $alpha)";
+$hslaColor = slothieHelpers()->HSLtoHSLA($page->backgroundColor()->escape());
 $style = "background-color: $hslaColor";
 ?>
 
 
 <body style="<?= $style ?>" class="h-full ">
-    <?php snippet('header') ?>
+<?php snippet('header') ?>
 
-    <main class="bg-white/10 h-full overflow-scroll ">
-        <div class="px-3 pt-1 lg:grid lg:grid-cols-6">
+<main class="bg-white/10 h-full overflow-scroll ">
+  <div class="px-3 pt-1 lg:grid lg:grid-cols-6">
 
-            <div class="button absolute col-start-1 font-serif text-sm all-small-caps hover:underline underline-offset-2">
-                <a href="<?= site()->url() ?>">Back</a>
-            </div>
-            <div class="col-start-2 col-span-4 text-center lg:text-lg text-md pt-16 lg:pt-24">
-                <?= $page->title()->kt() ?>
-            </div>
+    <div class="button absolute col-start-1 font-serif text-sm all-small-caps hover:underline underline-offset-2">
+      <a href="<?= site()->url() ?>">Back</a>
+    </div>
+    <div class="col-start-2 col-span-4 text-center lg:text-lg text-md pt-16 lg:pt-24">
+      <?= $page->title()->kt() ?>
+    </div>
 
-            <article class="lg:row-start-2 lg:col-start-2 lg:col-end-6 lg:text-lg text-md pt-24 pb-16">
-                <?= $page->description()->kt() ?>
-            </article>
-
-
-
-            <!-- ADDITIONAL INFO -->
-            <div class="lg:col-start-2 lg:col-span-4 font-serif">
-                <?php if ($page->presskits()->kt()->isNotEmpty()): ?>
-                    <h3 class="all-small-caps pt-6 text-sm">presskits</h3>
-                    <?php
-                    $presskits = $page->presskits()->toStructure();
-                    foreach ($presskits as $linkObject):
-                        ?>
-                        <div class="flex flex-row gap-3 text-base">
-                            <a href="<?= $linkObject->link()->toUrl() ?>" <?= $linkObject->target()->toBool() ? 'target="_blank"' : '' ?>> <?= $linkObject->title()->or($linkObject->link()) ?>
-                            </a>
-                        </div>
-                        <?php
-                    endforeach;
-                    ?>
-                <?php endif; ?>
+    <article class="lg:row-start-2 lg:col-start-2 lg:col-end-6 lg:text-lg text-md pt-24 pb-16">
+      <?= $page->description()->kt() ?>
+    </article>
 
 
-                <?php
-                $items = $page->additionalInfo()->toStructure();
-                foreach ($items as $item): ?>
-                    <div class="flex flex-col">
-                        <h3 class="font-serif text-sm all-small-caps pt-4"><?= $item->additionalDetails()->kt() ?></h3>
-                        <div class="flex flex-row gap-5 text-base">
-                            <?php if ($item->date()->kt()->isNotEmpty()): ?>
-                                <div class="flex flex-row gap-0.5"><?= $item->date()->kt() ?>–<?= $item->dateUntil()->kt() ?>
-                                </div>
-                            <?php endif; ?>
-                            <?= $item->value()->kt() ?>
-                        </div>
-                    </div>
-                <?php endforeach ?>
-            </div>
+    <!-- ADDITIONAL INFO -->
+    <div class="lg:col-start-2 lg:col-span-4 font-serif">
+      <?php if ($page->presskits()->kt()->isNotEmpty()): ?>
+        <h3 class="all-small-caps pt-6 text-sm">presskits</h3>
+        <?php
+        $presskits = $page->presskits()->toStructure();
+        foreach ($presskits as $linkObject):
+          ?>
+          <div class="flex flex-row gap-3 text-base">
+            <a
+              href="<?= $linkObject->link()->toUrl() ?>" <?= $linkObject->target()->toBool() ? 'target="_blank"' : '' ?>> <?= $linkObject->title()->or($linkObject->link()) ?>
+            </a>
+          </div>
+        <?php
+        endforeach;
+        ?>
+      <?php endif; ?>
 
-            <!-- BILDER -->
-            <section class="py-24 lg:col-start-2 lg:col-span-4">
-                <div class="flex flex-wrap last:pb-0 w-full gap-1 ">
-                    <?php foreach ($page->cover()->toFiles() as $image): ?>
-                        <figure class="h-52 lg:h-96">
-                            <?php echo $image->thumb([
-                                'quality' => 90,
-                                'format' => 'webp',
-                            ])->html(); ?>
-                        </figure>
-                    <?php endforeach ?>
-    
-                    <?php foreach ($page->gallery()->toFiles() as $image): ?>
-                        <figure class=" h-52 lg:h-96">
-                            <?php echo $image->thumb([
-                                'quality' => 90,
-                                'format' => 'webp',
-                            ])->html(); ?>
-                        </figure>
-                    <?php endforeach ?>
-                </div>
-            </section>
+
+      <?php
+      $items = $page->additionalInfo()->toStructure();
+      foreach ($items as $item): ?>
+        <div class="flex flex-col">
+          <h3 class="font-serif text-sm all-small-caps pt-4"><?= $item->additionalDetails()->kt() ?></h3>
+          <div class="flex flex-row gap-5 text-base">
+            <?php if ($item->date()->kt()->isNotEmpty()): ?>
+              <div class="flex flex-row gap-0.5"><?= $item->date()->kt() ?>–<?= $item->dateUntil()->kt() ?>
+              </div>
+            <?php endif; ?>
+            <?= $item->value()->kt() ?>
+          </div>
         </div>
-    </main>
-    <?= vite()->js('index.js') ?>
+      <?php endforeach ?>
+    </div>
+
+    <!-- BILDER -->
+    <section class="py-24 lg:col-start-2 lg:col-span-4">
+      <div class="flex flex-wrap last:pb-0 w-full gap-1 ">
+        <?php foreach ($page->cover()->toFiles() as $image): ?>
+          <figure class="h-52 lg:h-96">
+            <?php echo $image->thumb([
+              'quality' => 90,
+              'format' => 'webp',
+            ])->html(); ?>
+          </figure>
+        <?php endforeach ?>
+
+        <?php foreach ($page->gallery()->toFiles() as $image): ?>
+          <figure class=" h-52 lg:h-96">
+            <?php echo $image->thumb([
+              'quality' => 90,
+              'format' => 'webp',
+            ])->html(); ?>
+          </figure>
+        <?php endforeach ?>
+      </div>
+    </section>
+  </div>
+</main>
+<?= vite()->js('index.js') ?>
 </body>
 
 <script>
-    function correctFigureSize(figureElement) {
-        figureElement.onload = function () {
-            const aspectRatio = figureElement.naturalWidth / figureElement.naturalHeight;
-            const containerHeight = figureElement.closest('figure').clientHeight;
-            figureElement.closest('figure').style.width = `${containerHeight * aspectRatio}px`;
-        };
-    }
+  function correctFigureSize(figureElement) {
+    figureElement.onload = function () {
+      const aspectRatio = figureElement.naturalWidth / figureElement.naturalHeight;
+      const containerHeight = figureElement.closest('figure').clientHeight;
+      figureElement.closest('figure').style.width = `${containerHeight * aspectRatio}px`;
+    };
+  }
 </script>
 
 </html>
