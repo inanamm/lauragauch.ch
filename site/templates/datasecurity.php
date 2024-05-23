4bf9e05a408ee606a6fd78d3981917e3767d70d7
Martina <martina.holenstein@gmail.com>
2024-05-23 17:13:16 +0200

    fix datasecurity

--- site/templates/datasecurity.php
+++ site/templates/datasecurity.php
@@ -3,15 +3,29 @@
 
 <?php snippet('head') ?>
 
-<body class="grid pt-1 font-sans px-3">
-    <main>
-        <h1>
-        <?= $site->page('about')->datasecurity() ?>
+<body class="h-full no-scrollbar">
+    <?php snippet('header') ?>
+
+    <main class="bg-white/10 h-full overflow-scroll no-scrollbar">
+        <div class="px-3 pt-1 lg:grid lg:grid-cols-6">
+
+            <a href="<?= site()->url() ?>" class="font-sans lg:text-lg text-md fixed top-1 right-3 z-30 hover:no-underline">
+                Laura Gauch
+            </a>
+
+            <div
+                class="button absolute col-start-1 font-serif text-sm all-small-caps hover:underline underline-offset-2">
+                <a href="<?= site()->url() ?>">Back</a>
+            </div>
+            <h1 class="col-start-2 col-span-4 text-center lg:text-lg text-md pt-16 lg:pt-24">
+                <?= $page->title()->kt() ?>
             </h1>
-        <div class="sr-only">
-            <h2></h2>
-        </div>
-        <?= $page->privacyPolicy()->kirbytextinline() ?>
+            <h2 class="sr-only"> <?= $page->title()->kt() ?></h2>
+
+
+            <article class="lg:row-start-2 lg:col-start-2 lg:col-end-6 font-serif pt-24 pb-16">
+                <?= $site->page('datasecurity')->datasecurity()->kt() ?>
+            </article>
     </main>
 
     <?php snippet('seo/schemas'); ?>
