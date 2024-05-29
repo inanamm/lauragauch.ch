c3e4d54a6d9badd2e1c895170fcc293b2d2f66eb
Guido Offermans <dev@guidooffermans.com>
2024-05-29 14:21:28 +0200

    example dropdown part 2

--- site/snippets/projectInfo.php
+++ site/snippets/projectInfo.php
@@ -7,11 +7,51 @@
 
     <?php snippet('dropdown') ?>
 
-    <!--  example dropdown  -->
-    <div x-data="{ expanded: false }" class="relative my-4">
-      <button @click="expanded = ! expanded">Toggle Content</button>
+    <div
+      x-data="{
+            open: false,
+            toggle() {
+                if (this.open) {
+                    return this.close()
+                }
 
-      <div x-show="expanded" x-collapse class="absolute my-2 overflow-hidden">
+                this.$refs.button.focus()
+
+                this.open = true
+            },
+            close(focusAfter) {
+                if (! this.open) return
+
+                this.open = false
+
+                focusAfter && focusAfter.focus()
+            }
+        }"
+      x-on:keydown.escape.prevent.stop="close($refs.button)"
+      x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+      x-id="['dropdown-button']"
+      class="relative"
+    >
+      <button
+        x-ref="button"
+        x-on:click="toggle()"
+        :aria-expanded="open"
+        :aria-controls="$id('dropdown-button')"
+        type="button"
+        class="flex items-center gap-2 px-5 py-2.5"
+      >
+        Toggle me
+      </button>
+
+      <div
+        x-ref="panel"
+        x-show="open"
+        x-transition.origin.top.left
+        x-on:click.outside="close($refs.button)"
+        :id="$id('dropdown-button')"
+        style="display: none;"
+        class="absolute left-0 mt-2 w-40"
+      >
         <?php snippet(
           'copyToClipboard',
           ["copyText" => $page->url(), "buttonText" => "Share this project"]
