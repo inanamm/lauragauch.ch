efe6a9983529c34965af131d496762ff0eda87dc
Guido Offermans <dev@guidooffermans.com>
2026-01-02 15:34:43 +0100

    clean vite config and npm packages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import {globSync} from "glob";
-import {resolve} from "path";
 import kirby from "vite-plugin-kirby";
 import tailwindcss from "@tailwindcss/vite";
-
-const input = globSync(["src/index.{js,css}", "src/templates/*.{js,css}"]).map(
-  (path) => resolve(process.cwd(), path)
-);
 
 export default ({mode}) => ({
   root: "src",
   base: mode === "development" ? "/" : "/dist/",
 
-  resolve: {
-    alias: [{find: "@", replacement: resolve(__dirname, "src")}],
-  },
-
   build: {
-    outDir: resolve(process.cwd(), "public_html/dist"),
+    outDir: "../public_html/dist",
     emptyOutDir: true,
-    rollupOptions: {input},
+    rollupOptions: {
+      input: globSync("src/{index,templates/*}.{js,css}"),
+    },
   },
 
   plugins: [tailwindcss(), kirby()],
