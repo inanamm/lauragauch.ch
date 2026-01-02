398668695be3b23cfa390b2c08c25dcd9e507f0f
Guido Offermans <dev@guidooffermans.com>
2026-01-02 17:21:31 +0100

    format file

--- site/snippets/projectindex.php
+++ site/snippets/projectindex.php
@@ -8,40 +8,56 @@
   <button
     @click="menuOpen = !menuOpen; $nextTick(() => { setTimeout(() => $refs.projectIndexOverlay.scrollTop = 0, 200); $refs.projectIndexOverlay.style.backgroundColor = 'bg-white/50' })"
     class="fixed
-		bottom-2.5 lg:top-2 lg:bottom-auto
-		right-3 lg:left-3 lg:right-auto
-		font-serif all-small-caps text-sm
-		lg:hover:underline lg:underline-offset-2
-		rounded-lg lg:rounded-none
-		bg-white/65 lg:bg-transparent dark:bg-white/25 dark:lg:bg-transparent
-		px-2 lg:p-0
-		py-0.5
-		backdrop-blur-xs lg:backdrop-blur-0 cursor-crosshair" :aria-expanded="menuOpen" aria-controls="navigation"
-    aria-label="Navigation Menu">
+      bottom-2.5 lg:top-2 lg:bottom-auto
+      right-3 lg:left-3 lg:right-auto
+      font-serif all-small-caps text-sm
+      lg:hover:underline lg:underline-offset-2
+      rounded-lg lg:rounded-none
+      bg-white/65 lg:bg-transparent dark:bg-white/25 dark:lg:bg-transparent
+      px-2 lg:p-0
+      py-0.5
+      backdrop-blur-xs lg:backdrop-blur-0 cursor-crosshair"
+    :aria-expanded="menuOpen"
+    aria-controls="navigation"
+    aria-label="Navigation Menu"
+  >
     index
   </button>
 
-  <button @click="menuOpen = !menuOpen" x-show="menuOpen" x-transition:enter.delay.800ms class="fixed bottom-2.5 lg:top-2 lg:bottom-auto
-		right-3 lg:left-3 lg:right-auto
-		font-serif all-small-caps text-sm
-		lg:hover:underline lg:underline-offset-2
-		rounded-lg lg:rounded-none
-		bg-white/65 lg:bg-transparent dark:bg-white/25 dark:lg:bg-transparent
-		px-2 lg:p-0
-		py-0.5
-		z-50
-		backdrop-blur-xs lg:backdrop-blur-0 cursor-crosshair" :aria-expanded="menuOpen" aria-controls="navigation"
-          aria-label="Navigation Menu">
+  <button
+    @click="menuOpen = !menuOpen"
+    x-show="menuOpen"
+    x-transition:enter.delay.800ms
+    class="fixed
+      bottom-2.5 lg:top-2 lg:bottom-auto
+      right-3 lg:left-3 lg:right-auto
+      font-serif all-small-caps text-sm
+      lg:hover:underline lg:underline-offset-2
+      rounded-lg lg:rounded-none
+      bg-white/65 lg:bg-transparent dark:bg-white/25 dark:lg:bg-transparent
+      px-2 lg:p-0
+      py-0.5
+      z-50
+      backdrop-blur-xs lg:backdrop-blur-0 cursor-crosshair"
+    :aria-expanded="menuOpen"
+    aria-controls="navigation"
+    aria-label="Navigation Menu"
+  >
     close
   </button>
 
-  <div id="index" x-show="menuOpen" x-ref="projectIndexOverlay"
-       class="h-full w-full flex flex-col fixed bottom-0 left-0 backdrop-blur-md bg-white/50 dark:bg-black/50 overflow-y-scroll no-scrollbar"
-       x-transition:enter="transition lg:duration-1000 duration-700 ease-in-out"
-       x-transition:enter-start="translate-y-full"
-       x-transition:enter-end="translate-y-0" x-transition:leave="transition ease-in-out lg:duration-1000 duration-700 "
-       x-transition:leave-start="translate-y-0" x-transition:leave-end="translate-y-full">
-
+  <div
+    id="index"
+    x-show="menuOpen"
+    x-ref="projectIndexOverlay"
+    class="h-full w-full flex flex-col fixed bottom-0 left-0 backdrop-blur-md bg-white/50 dark:bg-black/50 overflow-y-scroll no-scrollbar"
+    x-transition:enter="transition lg:duration-1000 duration-700 ease-in-out"
+    x-transition:enter-start="translate-y-full"
+    x-transition:enter-end="translate-y-0"
+    x-transition:leave="transition ease-in-out lg:duration-1000 duration-700"
+    x-transition:leave-start="translate-y-0"
+    x-transition:leave-end="translate-y-full"
+  >
 
     <article class="top px-3 font-serif pt-32 pb-20">
       <div
@@ -63,10 +79,15 @@
                   <?= $project->title()->or($project->link()) ?>, <?= $project->year()->escape() ?>
                 </h2>
                 <figure class="relative h-24 lg:h-32 aspect-video">
-                  <iframe id="vimeo-iframe" class="absolute inset-0 w-full h-full"
-                          src="https://player.vimeo.com/video/<?= $project->vimeo()->escape() ?>?title=0&byline=0&portrait=0&autopause=0"
-                          frameborder="0"
-                          webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                  <iframe
+                    id="vimeo-iframe"
+                    class="absolute inset-0 w-full h-full"
+                    src="https://player.vimeo.com/video/<?= $project->vimeo()->escape() ?>?title=0&byline=0&portrait=0&autopause=0"
+                    frameborder="0"
+                    webkitallowfullscreen
+                    mozallowfullscreen
+                    allowfullscreen
+                  ></iframe>
                 </figure>
               </div>
               <?php
@@ -111,6 +132,7 @@
         ?>
       </div>
     </article>
+
   </div>
 </div>
 
